594f30bc1bb1092a81c79c5316ce5f57713b7a1e
amey-kudari <work.ameykudari@gmail.com>
2021-08-04 00:03:46 +0530

    init commit 2

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import logo from './logo.svg';
+import './App.css';
+import * as mdb from 'mdb-ui-kit'; // lib
+import { Input } from 'mdb-ui-kit'; // module
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar';
+import { AiOutlineLeft, AiOutlineArrowLeft } from "react-icons/ai";
+import { GiHamburgerMenu } from "react-icons/gi";
+function App() {
+  const [shownav, setShownav] = useState(true);
+  function toggleNav() {
+    setShownav(!shownav);
+  }
+  return (
+    <div className="App">
+      <div className="Content">
+        <div
+          className="sidebar"
+          style={
+            shownav
+              ? { width: '16rem', opacity: '1', transition: 'all 0.1s ease-out' }
+              : {
+                  width: '0',
+                  transform: 'translateX(-16rem)',
+                  transition: 'all 0.1s ease-out',
+                }
+          }>
+          <button
+            className="sidebar-button-2"
+            onClick={toggleNav}>
+              <AiOutlineLeft />
+          </button> <hr />
+          <p className="sidebar-text button-text"> WQI </p>
+          <p className="sidebar-text button-text"> ET </p>
+          <p className="sidebar-text button-text"> DROUGHT </p>
+          {/* <p>ASDF</p> */}
+        </div>
+        <div id="Header" className="header">
+          <div 
+            style={
+              !shownav
+                ? { opacity: '1', transition: 'all 0.1s ease-out' }
+                : { opacity: '0', transition: 'all 0.1s ease-out' }
+            }>
+            {!shownav && (
+              <button
+                onClick={toggleNav} className="sidebar-button">
+                <GiHamburgerMenu />
+              </button>
+            )}
+          </div>
+          <p className="sidebar-text"> HYDROLOGY TOOL IIIT-H</p>
+          <button
+            className="sidebar-button-1">
+            <AiOutlineArrowLeft />
+          </button>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,8 @@
+import '../App.css';
+
+function Sidebar({toggleNav}){
+  return (<>
+    <button className="sidebar-button-3">SIDEBAR CONTENT</button>
+  </>)
+}
+export default Sidebar;

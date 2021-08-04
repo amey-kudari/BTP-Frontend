b9800539f6c7675b9b2f87fd78a00621e2e28caa
amey-kudari <work.ameykudari@gmail.com>
2021-08-04 12:09:34 +0530

    redo the app

--- src/App.js
+++ src/App.js
@@ -1,58 +1,23 @@
-// import './App.css';
-// import React, { useState } from 'react';
-// import { AiOutlineLeft, AiOutlineArrowLeft } from "react-icons/ai";
-// import { GiHamburgerMenu } from "react-icons/gi";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
-  // const [shownav, setShownav] = useState(true);
-  // function toggleNav() {
-  //   setShownav(!shownav);
-  // }
   return (
     <div className="App">
-      {/* <div className="Content">
-        <div
-          className="sidebar"
-          style={
-            shownav
-              ? { width: '16rem', opacity: '1', transition: 'all 0.1s ease-out' }
-              : {
-                  width: '0',
-                  transform: 'translateX(-16rem)',
-                  transition: 'all 0.1s ease-out',
-                }
-          }>
-          <button
-            className="sidebar-button-2"
-            onClick={toggleNav}>
-              <AiOutlineLeft />
-          </button>
-          <p className="sidebar-text button-text"> WQI </p>
-          <p className="sidebar-text button-text"> ET </p>
-          <p className="sidebar-text button-text"> DROUGHT </p>
-        </div>
-        <div id="Header" className="header">
-          <div 
-            style={
-              !shownav
-                ? { opacity: '1', transition: 'all 0.1s ease-out' }
-                : { opacity: '0', transition: 'all 0.1s ease-out' }
-            }>
-            {!shownav && (
-              <button
-                onClick={toggleNav} className="sidebar-button">
-                <GiHamburgerMenu />
-              </button>
-            )}
-          </div>
-          <p className="sidebar-text"> HYDROLOGY TOOL IIIT-H</p>
-          <button
-            className="sidebar-button-1">
-            <AiOutlineArrowLeft />
-          </button>
-        </div>
-      </div>
-      <div></div> */}
-      <h1>APP</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/components/Sidebar.js
+++
@@ -1,8 +0,0 @@
-import '../App.css';
-
-function Sidebar(){
-  return (<>
-    <button className="sidebar-button-3">SIDEBAR CONTENT</button>
-  </>)
-}
-export default Sidebar;
